fd7bfe0e2360b3aee4269b36edb2e9b2b4ec71c8
Sowmya7501 <sowmya7501@gmail.com>
2021-03-21 22:28:44 +0530

    change made

--- final_project/3sum.go
+++ final_project/3sum.go
@@ -2,14 +2,14 @@ func threeSum(nums []int) [][]int {
     var a [][]int                                   //Team 2:
     n := len(nums)                                  //SRN :     PES1UG19CS497
     sort.Ints(nums)                                 //Name :    Sowmya M
-
+	
 	for i:=0;i<(n-2);i++ {
 		if (i>0) && (nums[i]==nums[i-1]) {
 			continue;
 		}
 		l := i+1;
 		r := n-1;
-
+		
 		for l<r {
 			sum := nums[i]+nums[l]+nums[r]
 			if sum<0 {
@@ -18,11 +18,11 @@ func threeSum(nums []int) [][]int {
 				r--;
 			} else {
 				a=append(a,[]int{nums[i],nums[l],nums[r]})
-
-				for (nums[l]==nums[l+1]) {
+				
+                for (l<r) && (nums[l]==nums[l+1]) {
 					l++;
 				}
-				for (nums[r]==nums[r-1]) {
+                for (l<r) && (nums[r]==nums[r-1]) {
 					r--;
 				}
 				l++;
